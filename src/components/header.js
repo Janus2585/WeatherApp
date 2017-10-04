2be1532d775ef697283cb65b8019dcb19c64ab35
Janus2585 <bd.yhome@gmail.com>
2017-10-04 04:21:21 -0500

    added header image, and added interactive google maps

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default () => {
+	return (
+		<div className="header">
+			<img src="../../img/tree2.jpg" />
+			<div className="titleBox">
+				<div className="title">Five Day Weather Forecast</div>
+				<div className="subtitle">Built using ReactJS, Redux and Sparklines</div>
+			</div>
+		</div>
+	); 
+}
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from "../components/google_map";
+
 class WeatherList extends Component {
 	renderWeather(cityData) {
 		//to add a key in a React list, add it to the top level element in the list, and it must be a unique piece of data
-		
+		const name = cityData.city.name;
 		//each "main" object in the cityData JSON is assigned to the argument "weather"
 		//weather.main.temp is the temperature of the city at a point in time
 		//loop through these temperatures to generate the graph
-		const temps = cityData.list.map(weather => weather.main.temp)
-		console.log(temps);
+		const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => (9/5)*(temp-273)+32);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+		const { lon, lat } = cityData.city.coord;
 
 		return (
-			<tr key={cityData.city.name}>
-				<td>{cityData.city.name}</td>
+			<tr key={name}>
 				<td>
-					<Chart data={temps} color="orange" />
+					<GoogleMap lon={lon} lat={lat} />
+				</td>
+				<td>
+					<Chart data={temps} color="orange" units="F" />
+				</td>
+				<td>
+					<Chart data={pressures} color="green" units="hPa" />
+				</td>
+				<td>
+					<Chart data={humidities} color="blue" units="%" />
 				</td>
 			</tr>
 		);
@@ -27,9 +39,9 @@ class WeatherList extends Component {
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (F)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
 					</tr>
 				</thead>
 
