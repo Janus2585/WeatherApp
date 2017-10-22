3dd2f473b19f7e1c9d99535aca85db7a85055c5c
Janus2585 <bd.yhome@gmail.com>
2017-10-22 00:05:54 -0500

    removed table border, header styling

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,8 @@ export default () => {
 			<img src="../../img/tree2.jpg" />
 			<div className="titleBox">
 				<div className="title">Five Day Weather Forecast</div>
-				<div className="subtitle">Built using ReactJS, Redux and Sparklines</div>
+				<div className="subtitle">Built using React.js, Redux and Sparklines</div>
+				<div className="subtitle">Use Google Chrome for best performance. You may need to enable javascript at the top right of the screen</div>
 			</div>
 		</div>
 	); 
