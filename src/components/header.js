f8a6c2c943e88d147ccdfee39d3f7d8fffa9cb99
Janus2585 <bd.yhome@gmail.com>
2017-10-23 18:40:41 -0500

    Removed note to use google chrome and enable scripts. Now works on all browsers and uses only https for security

--- src/components/header.js
+++ src/components/header.js
@@ -8,7 +8,6 @@ export default () => {
 			<div className="titleBox">
 				<div className="title">Five Day Weather Forecast</div>
 				<div className="subtitle">Built using React.js, Redux and Sparklines</div>
-				<div className="subtitle">Use Google Chrome for best performance. You may need to enable javascript at the top right of the screen</div>
 			</div>
 		</div>
 	); 
