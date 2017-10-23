4c2865245af49c4a7c803907e8555f069944327e
Janus2585 <bd.yhome@gmail.com>
2017-10-23 18:33:36 -0500

    changed http to https for security

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = "491da83d3b47384793915c4cad583e24";
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 //fetchWeather is an action creator that retrieves the weather data
